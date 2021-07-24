d147f279df8ad5a0fcf1cf595635eac7044e7f00
Stephen Wong <swong4009@gmail.com>
2021-07-24 19:14:56 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,75 @@
-const inputEl = document.getElementById("movie-input");
-const searchEl = document.getElementById("search-button");
-const clearEl = document.getElementById("clear-history");
-const currentMovie = document.getElementById("movie-selected");
-const movies = document.getElementById("movies");
+// initiate page function, and define constants 
+const initPage = async() => {
+        const inputEl = document.getElementById("movie-input");
+        const searchEl = document.getElementById("search-button");
+        const clearEl = document.getElementById("clear-history");
+        const currentMovie = document.getElementById("movie-selected");
+        let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+        console.log('searchHistory', searchHistory);
 
-// initiate page function, and define constants
-const initPage = async () => {
-	let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-	console.log('searchHistory', searchHistory);
-
-	const res = await fetch(trendingMoviesUrlEndpoint);
-	const data = await res.json();
-	const movieTitles = await createElementsAndGetTitles(data.results);
-	console.log('titles', movieTitles)
-}
-// define API Key constant, and API Endpoint Contants
+        const res = await fetch(trendingMoviesUrlEndpoint);
+        const data = await res.json();
+        console.log('data', data);
+        const movieTitles = createElementsAndGetTitles(data);
+        console.log('titles', movieTitles)
+    }
+    // define API Key constant, and API Endpoint Contants
 const apiKey = "2e903f6b5d70f51dab346edfeb17bdd4";
 const trendingMoviesUrlEndpoint = "https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey;
 const moviePosterURL = "https://www.themoviedb.org/t/p/w220_and_h330_face";
 
+// init page
 initPage();
-
+// defined constant function to grab data from JSON from fwth reqeust, ns create an HTML element for each item in the array
 const createElementsAndGetTitles = (data) => {
-	return data.map(movie => {
-		var div = document.createElement('div');
-		div.className = 'column is-one-quarter';
-		div.innerHTML = '<div class="card">' +
-			'  <div class="card-image">\n' +
-			'    <figure class="image is-4by3">\n' +
-			'      <img src="' + moviePosterURL + movie.backdrop_path + '" alt="Placeholder image">\n' +
-			'    </figure>\n' +
-			'  </div>' +
-			'<div class="card-content">' +
-			'<h2>' + movie.original_title + '</h2>' +
-			'</div>' +
-			' <footer class="card-footer">' +
-			'<p class="card-footer-item">' +
-			'<span>Find <a onclick="getReviews(\'' + movie.original_title + '\')">Trailers & Media</a>' +
-			'</span>' +
-			'</p>' +
-			'</footer></div>';
-		this.movies.appendChild(div);
-		return {title: movie.original_title, url: `${moviePosterURL}${movie.backdrop_path}`};
-	})
+    console.log('movies', data);
+    return data.map(movie => {
+        const movieEl = document.createElementById(`movieEl-${movie.title}`)
+        return { title: movie.original_title, url: `${moviePosterURL}${movie.backdrop_path}` };
+    })
 };
 
-const getReviews = (movieName) => {
-	console.log('Clicked movie: ' + movieName);
-}
+// const getMovieReviews = (data) => {
+// console.log('movie reviews', data);
+// return data.map(reviews => {
+//  const movieReviewsEl = document.createElement(`movieReviewsEl-${reviews.title}`)
+// return ()
 
-//const getMovieReviews = (data) => {
-//console.log('movie reviews', data);
-//return data.map(reviews => {
-//const movieReviewsEl = document.createElement(`movieReviewsEl-${reviews.title}`)
-//  return ()
+// }
+// };
 
-//}
-//}
+// --- stephen
+
+// function getQuote() {
+// fetch("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + Math.random())
+// .then(response => response.json())
+// .then(function(data) {
+// console.log(data);
+
+// data.filter(key => {
+
+// let quote = document.querySelector(".quote");
+// let author = document.querySelector(".author");
+// let cleanQuote = key.content.replace(/<\/?p[^>]*>/g, ''); // This way we remove <p> tag from quote (api has quotes with p tags)
+
+// let share = 'https://twitter.com/home?status=' + cleanQuote + ' Author: ' + key.title;
+// console.log(share)
+
+// quote.innerHTML = key.content;
+// author.innerHTML = key.title;
+
+// document.getElementById('twitterShare').href=share;
+// });
+
+// })
+// .catch(function(error) {
+// If there is any error you will catch them here
+// console.log(error);
+// });
+// }
+
+// const newQuote = document.getElementById('newQuote')
+// newQuote.addEventListener('click', getQuote); // new quote on button click
+// window.onload = getQuote; // new quote on page load
+
+// add event listener when click, on call click being added - wong
