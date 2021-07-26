8aa081f9b65fa5aa6a6e9f66bff694f63c1df96d
Michael Groberman <michaelgroberman@MacBook-Pro.local>
2021-07-26 15:54:35 -0400

    updated script.js to load google youtube search api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@ const clearEl = document.getElementById("clear-history");
 const currentMovie = document.getElementById("movie-selected");
 const movies = document.getElementById("movies");
 
+gapi.load("client", loadClient);
+  
+function loadClient() {
+    gapi.client.setApiKey("AIzaSyAw-kJfm7f9eOk4vpEI3pYEoBIZuP1txDU");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+                function(err) { console.error("Error loading GAPI client for API", err); });
+}
 // initiate page function, and define constants
 const initPage = async () => {
 	let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
