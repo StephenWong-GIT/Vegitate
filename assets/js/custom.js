971f3e3ecd315068585d8051068de3f1aba9e76d
Michael Groberman <michaelgroberman@MacBook-Pro.local>
2021-07-26 15:31:42 -0400

    updating out-of-sync files

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+gapi.load("client", loadClient);
+  
+function loadClient() {
+    gapi.client.setApiKey("YOUTUBE_API_KEY");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+                function(err) { console.error("Error loading GAPI client for API", err); });
